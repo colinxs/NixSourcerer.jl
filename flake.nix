670726b021376ab44e829eecef6f4b4da2baf4d7
Colin Summers <me@colinxsummers.com>
2021-04-18 02:12:07 -0600

    save

--- flake.nix
+++ flake.nix
@@ -25,37 +25,22 @@
       pkgs = inputs.nixpkgs.legacyPackages."${system}";
       murpkgs = inputs.nix-home.legacyPackages."${system}".mur;
       julia = murpkgs.julia.latest;
-      buildJuliaEnvironment = pkgs.callPackage ./juliaPlatform/buildJuliaEnvironment.nix { inherit julia; };
-      buildJuliaDepot = pkgs.callPackage ./juliaPlatform/buildJuliaDepot.nix { inherit julia; };
-      buildJuliaPackage = pkgs.callPackage ./juliaPlatform/buildJuliaPackage.nix { inherit buildJuliaEnvironment; };
-      buildOverrides= pkgs.callPackage ./juliaPlatform/buildOverrides.nix { };
       
-      juliaProjectFile=./testenv/Project.toml; 
-      juliaManifestFile=./testenv/Manifest.toml; 
-      juliaRegistries = [
-        inputs.general-registry
-        inputs.personal-registry
-      ];
 
-      hash = "sha256:0x5dxjkidfcs1kd1450cg4w05rgv1k1nvzjncrswdisvxsw5jh7z"; 
-    in {
-      defaultPackage."${system}" = buildJuliaDepot { 
-        juliaProjectFile=./testenv/Project.toml; 
-        juliaManifestFile=./testenv/Manifest.toml; 
-        juliaPackages = import ./testenv/NixManifest.nix { inherit pkgs; };
-        # sha256 = pkgs.lib.fakeSha256;
-        sha256 = "0n3fhd2fa0gh6fqvfnw710ahiysjcfr80jp24rzyh0ks6wz9qrgz";
+      callPackage = pkgs.lib.callPackagesWith ( pkgs // { inherit julia callPackage; } );
+      juliaPlatform = callPackage ./julia-platform {};
+      depot = juliaPlatform.buildJuliaPackage { 
+        src = ./.;
+        juliaRegistries = with inputs; [ general-registry personal-registry ];
+        sha256 = "1vbf4k5nck4wl73m09p6mbpr0f8rvkbcs21s02h3r9d07wsfwfx7";
       }; 
-      # defaultPackage."${system}" = buildJuliaPackage { 
-      #   inherit juliaRegistries;
-      #   src = ./.;
-      #   sha256 = pkgs.lib.fakeSha256;
-      # }; 
-      # defaultPackage."${system}" = buildOverrides { 
-      #   # juliaArtifactsFile = ./testenv/Artifacts.toml;
-      #   juliaArtifactsFiles = [./testenv/Artifacts.toml ./testenv/Artifacts2.toml];
-      #   # sha256 = pkgs.lib.fakeSha256;
-      # }; 
+    in {
+      defaultPackage."${system}" = juliaPlatform.buildJuliaWrapper {
+        inherit depot julia;
+        extraPackages = with pkgs; [ git nix nix-prefetch nixpkgs-fmt ];
+        temporaryDepot = false;
+        defaultDepots = true;
+      };
     };
 }
 
