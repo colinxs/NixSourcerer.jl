147629e9e6184986f15ca4d98e9edbbec4c6b23f
Colin Summers <me@colinxsummers.com>
2021-05-23 20:05:51 -0700

    debug

--- src/handlers/archive.jl
+++ src/handlers/archive.jl
@@ -10,7 +10,7 @@ function archive_handler(name::AbstractString, spec::AbstractDict)
     fetcher_name = builtin ? "builtins.fetchTarball" : "pkgs.fetchzip"
     fetcher_args = Dict{Symbol,Any}()
     fetcher_args[:url] = spec["url"]
-    # fetcher_args[:name] = sanitize_name(get(spec, "name", url_name(spec["url"])))
+    fetcher_args[:name] = sanitize_name(get(spec, "name", url_name(spec["url"])))
     fetcher_args[:sha256] = get_sha256(fetcher_name, fetcher_args)
 
     return Source(; pname=name, version=fetcher_args[:sha256], fetcher_name, fetcher_args)
