772f3b1f4d5104aa425a44028d9ac59c4b8b7287
Colin Summers <me@colinxsummers.com>
2021-11-30 19:41:57 -0700

    save

--- script/make.jl
+++ script/make.jl
@@ -4,21 +4,26 @@ Pkg.instantiate()
 using SnoopCompile
 using NixSourcerer
 
-tinf = @snoopi_deep begin
-    include(joinpath(pkgdir(NixSourcerer), "test", "runtests.jl"))
-end
-ttot, pcs = SnoopCompile.parcel(tinf)
+# tinf = @snoopi_deep begin
+#     include(joinpath(pkgdir(NixSourcerer), "test", "runtests.jl"))
+# end
+# ttot, pcs = SnoopCompile.parcel(tinf)
 
 using PackageCompiler
 mktempdir() do dir
-    SnoopCompile.write(dir, pcs; always=true)
-    files = readdir(dir; join=true)
+    # SnoopCompile.write(dir, pcs; always=true)
+    # files = readdir(dir; join=true)
     create_app(
         joinpath(@__DIR__, ".."),
         joinpath(@__DIR__, "..", "build");
-        app_name="nix-sourcerer",
-        precompile_statements_file=files,
-        audit=true,
+        executables = [
+            "nix-sourcerer" => "julia_main",
+        ],
+        # precompile_statements_file=files,
         force=true,
+        incremental=true,
+        include_transitive_dependencies=false,
+        filter_stdlibs = false,
+        cpu_target="native",
     )
 end
